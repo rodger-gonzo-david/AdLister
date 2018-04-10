bc75de55b6874f3a33eed1c33fb57f5a6ff24ef4
RodgerScott <rodger.scott@gmail.com>
2018-04-10 09:27:12 -0500

    removed souts, confirmed search is working

--- src/main/java/com/codeup/adlister/controllers/SearchServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchServlet.java
@@ -14,23 +14,14 @@ import java.util.List;
 @WebServlet(name = "SearchServlet", urlPatterns = "/ads/search")
 public class SearchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Is this working POST");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Is this working GET");
 
-//        if (request.getMethod().equalsIgnoreCase("get")) {
             String entry = request.getParameter("searchInput");
-        System.out.println("entry = " + entry);
             List<Ad> ads = DaoFactory.getAdsDao().searchedAds(entry);
-        for (Ad ad: ads
-             ) {
-            System.out.println("ad.getTitle() = " + ad.getTitle());
-            
-        }
             request.setAttribute("search", ads);
             request.getRequestDispatcher("/WEB-INF/ads/search.jsp").forward(request, response);
-//        }
     }
 }
