11ea3c6784a28cbec10a6b3c6b19897370f4f83c
davidalex620 <spand3r@gmail.com>
2018-04-11 14:13:43 -0500

    password and email are updating DB, null errors were fixed

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -32,15 +32,22 @@ public class ViewProfileServlet extends HttpServlet {
 
         String emailChange = request.getParameter("emailInput");
         String passwordChange = request.getParameter("passwordInput");
+        System.out.println(emailChange);
+        System.out.println(passwordChange);
         User user = (User) request.getSession().getAttribute("user");
         String username = user.getUsername();
 
-        if (!emailChange.equals("")) {
+//        if (!emailChange.equals("")) {
+        if (emailChange != null || emailChange.trim() != "") {
             DaoFactory.getUsersDao().modifyEmail(emailChange, username);
         }
 
-        if (!passwordChange.equals("")) {
+//        if (!passwordChange.equals("")) {
+        if (passwordChange != null || passwordChange.trim() != "") {
             DaoFactory.getUsersDao().modifyPassword(passwordChange, username);
+        } else if (passwordChange == null || passwordChange.trim() == ""){
+            System.out.println("I am null");
+            response.sendRedirect("/profile");
         }
 
             user = DaoFactory.getUsersDao().findByUsername(username);
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -11,15 +11,12 @@
 
     <div class="container">
         <h1>Welcome, ${sessionScope.user.username}!</h1>
-        <h3>Email: ${sessionScope.user.email}</h3>
         <form action="/profile" method="post">
+            <h3>Email: ${sessionScope.user.email}</h3>
             <label for="emailInput"><input type="text" name="emailInput" id="emailInput" placeholder="update email"></label>
-            <button type="submit" class="btn btn-primary">Edit Email</button>
-        </form>
-        <h3>Change Password</h3>
-        <form action="/profile" method="post">
+            <h3>Update Password</h3>
             <label for="passwordInput"><input type="text" name="passwordInput" id="passwordInput" placeholder="update password"></label>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Edit Email</button>
         </form>
         <c:forEach var="ad" items="${profileAds}">
             <div class="col-md-6">
