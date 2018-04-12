b2e219b67436226434253db8b7f384b2f76e6ee6
RodgerScott <rodger.scott@gmail.com>
2018-04-12 10:47:48 -0500

    completed search by category functionality

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -10,7 +10,7 @@ public interface Ads {
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
 
-    List<Ad> searchedAds(String s);
+    List<Ad> searchedAds(String s, String c);
 
 
     List<Ad> profileAds(String s);
